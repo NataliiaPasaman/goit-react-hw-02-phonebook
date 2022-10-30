6fb21cd495e06264ca960491d2a099c11fc19b43
NataliiaPasaman <natapasaman@gmail.com>
2022-10-30 22:15:54 +0200

    create component Form and render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        // display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React from "react";
+import { Component } from "react";
+import { nanoid } from 'nanoid';
+import { PhonebookForm } from "./PhonebookForm/PhonebookForm";
+import { ContactsList } from "./ContactsList/ContactsList";
+
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      contacts: [],
+      name: ''
+    }
+  }
+
+
+  onInputChange = (evt) => {
+    const { name, value } = evt.currentTarget;
+    this.setState({ [name]: value });
+  }
+
+
+  onSubmitForm = (evt) => {
+    evt.preventDefault();
+    const { name } = this.state;
+    const contactId = nanoid();
+    const contact = {
+      name,
+      id: contactId,
+    };
+    this.setState((prevState) => {
+       this.state = {
+        ...this.state,
+        contacts: prevState.contacts.push(contact),
+      }
+      console.log('prevState', prevState.contacts);
+    });
+
+    // console.log(`Name ${this.state.name}`);
+    console.log('contact', contact);
+  }
+
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          fontSize: 40,
+          color: '#010101'
+        }}
+      >
+        <PhonebookForm 
+        onInputChange={this.onInputChange}
+        onSubmitForm={this.onSubmitForm}
+        />
+        <ContactsList />
+      </div>
+    );
+  }
 };
---
+++ src/components/PhonebookForm/PhonebookForm.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import css from "components/PhonebookForm/PhonebookForm.module.css";
+
+export const PhonebookForm = ({ onInputChange, onSubmitForm }) => {
+  return (
+    <form className={css.formPhone} onSubmit={onSubmitForm}>
+      <label className={css.labelPhone}>
+        Name
+        <input
+          className={css.inputPhone}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={onInputChange}
+        />
+      </label>
+      <button className={css.btnPhone} type="submit">Add contact</button>
+    </form>
+  );
+};
+
+PhonebookForm.propTypes = {
+    onInputChange: PropTypes.func.isRequired,
+    onSubmitForm: PropTypes.func.isRequired,
+}
